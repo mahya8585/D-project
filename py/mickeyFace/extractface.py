0411b1b3766df139fc3ce460475a52374f1bc339
maayaj37 <ishida.m@opst.co.jp>
2017-12-19 13:12:26 +0900

    作成中

--- py/mickeyFace/extractface.py
+++ py/mickeyFace/extractface.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
+import sys
+print(sys.version_info)
+sys.path.append('../site-packages')
+
 import requests
 import json
 from bs4 import BeautifulSoup
 
-CHANNEL = '#CHANNEL'
-SLACK_TOKEN = 'xoxp-xxxxxxxxxxxxxxxxxxxxxxx'
+CHANNEL = '#xxxxxxxx'
+SLACK_TOKEN = 'xoxp-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 FILE_API = 'https://slack.com/api/files.list'
 SHARE_URL = 'https://slack.com/api/files.sharedPublicURL'
 REVOKE_URL = 'https://slack.com/api/files.revokePublicURL'
 
-# custom vision service image URL API
-CUSTOM_VISION_URL = 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/xxxxxxxxxxxxxxxxx/url?iterationId=xxxxxxxxxxxxxxxxxx3'
-CUSTOM_VISION_KEY = 'xxxxxxxxxxxxxxxx'
+# custom vision service image upload API
+CUSTOM_VISION_URL = 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/xxxxxxxxxxxxxxxxxxxxxxxxxx'
+CUSTOM_VISION_KEY = 'xxxxxxxxxxxxxxxxxxxxxxx'
 
 # slack
-INCOMING_URL = 'https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxx'
+INCOMING_URL = 'https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 
 def revoke_file_url(revoke_file_id):
@@ -32,6 +36,7 @@ def get_permalink_public():
     :return: permalink_public
     """
     public_file_info = requests.get(SHARE_URL + '?token=' + SLACK_TOKEN + '&file=' + file_id + '&pretty=1').json()
+    print(public_file_info)
     return public_file_info['file']['permalink_public']
 
 
@@ -41,25 +46,40 @@ def get_img_url(permalink_public):
     :param permalink_public: パブリック公開されたslack fileのURL
     :return: 画像URL
     """
-    target_img = requests.get(permalink_public, stream=True)
+    target_img = requests.get(permalink_public)
     img_soup = BeautifulSoup(target_img.text, 'html.parser')
     return img_soup.find('img')['src']
 
 
-def execute_custom_vision(target_img_url):
+def image_download(image_name):
     """
-    カスタムビジョンサービスの実行
-    :param target_img_url: 評価対象画像URL
+    画像をローカルダウンロードする
+    :param image_name:
     :return:
     """
-    azure_body = {
-        'Url': target_img_url
-    }
+    r = requests.get(image_url)
+    f = open(image_name, 'wb', buffering=0)
+    f.write(r.content)
+    f.close()
+    print('image downloaded!')
+
+
+def execute_custom_vision(image_file):
+    """
+    カスタムビジョンサービスの実行
+    :param image_file: 評価対象画像
+    :return:
+    """
+    # azure_body = {
+    #     'Url': target_img_url
+    # }
     azure_header = {
         'Prediction-Key': CUSTOM_VISION_KEY,
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/octet-stream'
     }
-    return requests.post(CUSTOM_VISION_URL, headers=azure_header, data=json.dumps(azure_body)).json()
+    # return requests.post(CUSTOM_VISION_URL, headers=azure_header, data=json.dumps(azure_body)).json()
+    print('azure run')
+    return requests.post(CUSTOM_VISION_URL, headers=azure_header, data=image_file).json()
 
 
 def create_message(azure_result):
@@ -68,6 +88,9 @@ def create_message(azure_result):
     :param azure_result:
     :return:
     """
+    if azure_result['Predictions'] is None:
+        return azure_result['Code']
+
     result_prediction = None
     for prediction in azure_result['Predictions']:
         probability = prediction['Probability']
@@ -110,22 +133,31 @@ def post_slack(message):
 
 # main処理スタート
 # file情報取得
+print('start')
 file_list = requests.get(FILE_API + '?token=' + SLACK_TOKEN + '&channel=' + CHANNEL + '&count=1&types=images').json()
 file_info = file_list['files'][0]
 file_id = file_info['id']
-title = file_info['title']
-file_type = file_info['filetype']
+file_name = file_info['title'] + file_info['filetype']
 
 # file パブリックURLの取得
 revoke_file_url(file_id)
 download_url = get_permalink_public()
 
 # azure custom vision service
-response = execute_custom_vision(get_img_url())
+image_url = get_img_url(download_url)
+image_download(file_name)
+
+image_file = open(file_name, 'rb', buffering=0)
+response = execute_custom_vision(image_file.read())
+image_file.close()
+print(response)
 
 # 評価結果の作成
+post_message = create_message(response)
 # slack通知
-post_slack(create_message(response))
+post_slack(post_message)
 
 # fileパブリック設定のクローズ
 revoke_file_url(file_id)
+
+print('finish.')
--- py/mickeyFace/readme.md
+++ py/mickeyFace/readme.md
@@ -1,6 +1,14 @@
 # ミッキーフェイス判定機 for slack
 
 Python2系で実施。
-
+下記をpip installしたライブラリ群(site-package.zip)をAzure Functionsのwwwroot直下に配置すること
 - BeautifulSoup4
 - requests
+
+Azure FunctionsはPythonのHTTPトリガー(GET)を利用
+
+### これから追加しないといけない機能
+- slack キックワードの設定
+- 画像サイズが大きすぎる場合のリサイズ処理
+- minetypeを使ったバリデーションチェック
+- ミニー判定
